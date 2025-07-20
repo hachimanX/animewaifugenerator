8b374c0b5c0c70ce6a42539ab1be22c8ceaab734
hachimanX <bashirmohammadusman@gmail.com>
2025-07-21 02:10:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const waifus = [
-  'waifus/waifu1.jpg',
-  'waifus/waifu2.jpg',
-  'waifus/waifu3.jpg',
-  'waifus/waifu4.jpg',
-  'waifus/waifu5.jpg'
+  'images/waifu1.jpg',
+  'images/waifu2.jpg',
+  'images/waifu3.jpg',
+  'images/waifu4.jpg',
+  'images/waifu5.jpg'
 ];
-function generateWaifu() {
-  const img = document.getElementById('waifuImage');
+
+const img = document.getElementById('waifu-img');
+const btn = document.getElementById('generate-btn');
+const loader = document.getElementById('loader');
+
+function showWaifu() {
+  loader.classList.remove('hidden');
   const index = Math.floor(Math.random() * waifus.length);
-  img.src = waifus[index];
+  setTimeout(() => {
+    img.src = waifus[index];
+    loader.classList.add('hidden');
+  }, 800);  // simulate load
 }
+
+btn.addEventListener('click', showWaifu);
+document.addEventListener('DOMContentLoaded', showWaifu);
